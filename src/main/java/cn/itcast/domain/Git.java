2c39d4c9a905e79986f86e3f70106dcd02fe9f4a
zhangluyang <zhangluyang2013@163.com>
2018-11-24 16:52:30 +0800

    添加git

---
+++ src/main/java/cn/itcast/domain/Git.java
@@ -0,0 +1,13 @@
+package cn.itcast.domain;
+
+public class Git {
+
+    public void test(){
+
+        System.out.println("hello git");
+        System.out.println("hello git");
+        System.out.println("hello git");
+        System.out.println("hello git");
+        System.out.println("hello git");
+    }
+}
