2c21cff80f0f344764ca5efe58994e5e4765459d
zhangluyang <zhangluyang2013@163.com>
2018-11-24 18:46:58 +0800

    更新方法了

--- src/main/java/cn/itcast/domain/Git.java
+++ src/main/java/cn/itcast/domain/Git.java
@@ -7,6 +7,11 @@ public class Git {
         test();
     }
 
+    //添加文档注释
+
+    /**
+     * 方法更新
+     */
     public static void test(){
 
         System.out.println("hello git");
@@ -14,5 +19,9 @@ public class Git {
         System.out.println("hello git");
         System.out.println("hello git");
         System.out.println("hello git");
+
+        //更新输出语句
+
+        System.out.println("你好,git");
     }
 }
