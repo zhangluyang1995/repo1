24c3df945e6131ed1ee13c76edbde782417c2b1c
zhangluyang <zhangluyang2013@163.com>
2018-11-24 18:41:47 +0800

    更新主方法

--- src/main/java/cn/itcast/domain/Git.java
+++ src/main/java/cn/itcast/domain/Git.java
@@ -2,7 +2,12 @@ package cn.itcast.domain;
 
 public class Git {
 
-    public void test(){
+    public static void main(String[] args) {
+
+        test();
+    }
+
+    public static void test(){
 
         System.out.println("hello git");
         System.out.println("hello git");
