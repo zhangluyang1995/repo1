4015be8bf56ff81e9c4b943a82df925f75d2cb91
zhangluyang <zhangluyang2013@163.com>
2018-11-25 14:11:40 +0800

    file 读写util

---
+++ src/main/java/cn/itcast/util/ArrayListdemo.java
@@ -0,0 +1,11 @@
+package cn.itcast.util;
+
+public class ArrayListdemo {
+
+    public static void main(String[] args) {
+
+        double pow = Math.pow(2, 100);
+
+        System.out.println(pow);
+    }
+}
