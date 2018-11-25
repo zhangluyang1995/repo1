a5cfd3b40938744c2f34386bf2af30076335778f
zhangluyang <zhangluyang2013@163.com>
2018-11-25 14:17:04 +0800

    数组创建的3种方式

--- src/main/java/cn/itcast/util/ArrayListdemo.java
+++ src/main/java/cn/itcast/util/ArrayListdemo.java
@@ -9,6 +9,7 @@ public class ArrayListdemo {
         int [] arr1 = new int [30];  //数组创建1
         int [] arr2 = {36,42};//数组创建2
         int [] arr3 = new int []{1,2,3,4,5} ; //数组创建3
+
         System.out.println(pow);
     }
 }
