e4d19dee6aefda4c787a68ae5cc0eeba416f2e97
zhangluyang <zhangluyang2013@163.com>
2018-11-25 14:16:33 +0800

    file 读写util

--- src/main/java/cn/itcast/util/ArrayListdemo.java
+++ src/main/java/cn/itcast/util/ArrayListdemo.java
@@ -6,6 +6,9 @@ public class ArrayListdemo {
 
         double pow = Math.pow(2, 100);
 
+        int [] arr1 = new int [30];  //数组创建1
+        int [] arr2 = {36,42};//数组创建2
+        int [] arr3 = new int []{1,2,3,4,5} ; //数组创建3
         System.out.println(pow);
     }
 }
